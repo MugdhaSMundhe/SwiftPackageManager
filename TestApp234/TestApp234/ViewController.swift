101a64e79dcd9e923484b4dff70df5153cfd625b
MugdhaSMundhe <89035654+MugdhaSMundhe@users.noreply.github.com>
2025-02-14 18:01:27 +0530

    XCFrameworks added

---
+++ TestApp234/TestApp234/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp234
+//
+//  Created by Mugdha Mundhe on 14/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestApp234/TestApp234Tests/TestApp234Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp234Tests.swift
+//  TestApp234Tests
+//
+//  Created by Mugdha Mundhe on 14/02/25.
+//
+
+import Testing
+@testable import TestApp234
+
+struct TestApp234Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
