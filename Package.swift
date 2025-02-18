767f4e2adfae595869d30d8a97e8612551eb4828
Mugdha Mundhe <89035654+MugdhaSMundhe@users.noreply.github.com>
2025-02-18 18:31:42 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,13 +8,13 @@ let package = Package(
     products: [
         .library(
             name: "ContactCenterMessagingApp",
-            targets: ["ContactCenterMessagingSDK"]),
+            targets: ["ContactCenterMessaging"]),
     ],
     targets: [
         .target(
             name: "ContactCenterMessagingApp"),
         .binaryTarget(
-            name: "ContactCenterMessagingSDK",
+            name: "ContactCenterMessaging",
             url: "https://github.com/microsoft/ContactCenterMessagingSDK-ios/releases/download/v0.0.2/ContactCenterMessagingSDK.zip",
             checksum: "b30812cf887c967b37fa84027a82a1de0559ccfa3c1d942dc15357986f65e3c2"
         )
